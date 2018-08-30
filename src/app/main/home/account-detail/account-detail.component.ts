0d7bbd383184eb4173b43e707f190418b1378d6e
manu-cr <manuelcidrodriguez@gmail.com>
2018-08-30 15:29:50 +0200

    Update app

--- src/app/main/home/account-detail/account-detail.component.ts
+++ src/app/main/home/account-detail/account-detail.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, Inject, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Location } from '@angular/common';
-import { OChartComponent } from 'ontimize-web-ngx-charts';
-import { FilterExpressionUtils, OFormComponent, OListComponent } from 'ontimize-web-ngx';
+import { OChartComponent, ChartDataAdapter } from 'ontimize-web-ngx-charts';
+import { Codes, FilterExpressionUtils, OFormComponent, OListComponent, OntimizeEEService } from 'ontimize-web-ngx';
+declare var d3: any;
 
 @Component({
   selector: 'account-detail',
@@ -10,6 +11,7 @@ import { FilterExpressionUtils, OFormComponent, OListComponent } from 'ontimize-
   host: {
     '[class.account-detail]': 'true'
   },
+  providers: [OntimizeEEService],
   encapsulation: ViewEncapsulation.None
 })
 export class AccountDetailComponent implements AfterViewInit {
@@ -18,43 +20,95 @@ export class AccountDetailComponent implements AfterViewInit {
   @ViewChild('chart') chart: OChartComponent;
   @ViewChild('list') list: OListComponent;
   today = new Date();
-  date = new Date(2000, 5);
-  currentMonth: number = + new Date(2000, 5).getMonth();
+  date = new Date(2000, 5, 1);
+  currentMonth: number = + new Date(2000, 5, 1).getMonth();
 
   constructor(
+    protected service: OntimizeEEService,
     public location: Location
   ) { }
 
   ngAfterViewInit(): void {
     this.chart.options.chart['showLegend'] = false;
     this.chart.options.chart.xAxis.axisLabel = '';
+    this.chart.options.chart.yAxis.axisLabel = '';
+    this.chart.options.chart.yAxis.tickFormat = (d) => {
+      let format = d3.format(',d');
+      return format(d) + ' €';
+    };
     this.chart.options.chart['color'] = ['#42a5f5'];
     this.chart.options.chart.lines = { isArea: true }
-    // this.chart.options.chart['showYAxisLabel'] = false;
-    // this.chart.options.chart['showXAxisLabel'] = false;
+    // this.chart.options.chart.forceY = [0];
   }
 
-  queryChart(): void {
+  queryData(): void {
+    this.chart.data = [];
     let feKey = FilterExpressionUtils.buildExpressionFromObject(this.form.getKeysValues());
     let fe1 = FilterExpressionUtils.buildExpressionMoreEqual('DATE_', new Date(this.date.getFullYear(), this.date.getMonth(), 1).getTime());
     let fe2 = FilterExpressionUtils.buildExpressionLess('DATE_', new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getTime());
     let beDates = FilterExpressionUtils.buildComplexExpression(fe1, fe2, FilterExpressionUtils.OP_AND);
-    let be = FilterExpressionUtils.buildBasicExpression(FilterExpressionUtils.buildComplexExpression(feKey, beDates, FilterExpressionUtils.OP_AND));
 
-    this.chart.queryData(be, { sqltypes: { DATE_: 93 } });
-    this.list.queryData(be, { sqltypes: { DATE_: 93 } });
+    let self = this;
+    let beMonthEnd = FilterExpressionUtils.buildBasicExpression(FilterExpressionUtils.buildComplexExpression(feKey, fe2, FilterExpressionUtils.OP_AND));
+    this.service.configureService(this.service.getDefaultServiceConfiguration('movements'));
+    this.service.query(beMonthEnd, ['MOVEMENT', 'DATE_'], 'movement', { DATE_: 93 }).subscribe(res => {
+      if (res && res.code === Codes.ONTIMIZE_SUCCESSFUL_CODE && res.data.length > 0) {
+        let balance = res.data.map(a => a.MOVEMENT).reduce((a, b) => a + b);
+        let balanceProgress = [{ BALANCE: balance, DATE: new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getTime() }];
+        let beCurrentMonth = FilterExpressionUtils.buildBasicExpression(FilterExpressionUtils.buildComplexExpression(feKey, beDates, FilterExpressionUtils.OP_AND));
+        self.service.query(beCurrentMonth, ['MOVEMENT', 'DATE_'], 'movement', { DATE_: 93 }).subscribe(res => {
+          if (res && res.code === Codes.ONTIMIZE_SUCCESSFUL_CODE && res.data.length > 0) {
+            balanceProgress.unshift(...res.data.slice().reverse().map(elem => {
+              return { BALANCE: (balance -= elem.MOVEMENT), DATE: elem.DATE_ };
+            }).reverse());
+            balanceProgress.unshift({ BALANCE: balance, DATE: new Date(this.date.getFullYear(), this.date.getMonth(), 1).getTime() });
+            let factory = self.chart.getChartDataAdapterFactory();
+            let adapter: ChartDataAdapter = factory.getAdapter('line');
+            let adaptedResult = adapter.adaptResult(balanceProgress);
+            self.chart.data = adaptedResult;
+          }
+        }, err => {
+          console.log(err);
+        });
+      }
+    }, err => {
+      console.log(err);
+    });
+
+    let beList = FilterExpressionUtils.buildBasicExpression(FilterExpressionUtils.buildComplexExpression(feKey, beDates, FilterExpressionUtils.OP_AND));
+    this.list.queryData(beList, { sqltypes: { DATE_: 93 } });
   }
 
   onClickPrevMonth(): void {
-    this.date.setMonth(this.currentMonth - 1);
-    this.currentMonth = this.date.getMonth();
-    this.queryChart();
+    this.currentMonth = this.getPrevMonth(this.currentMonth);
+    this.date.setMonth(this.currentMonth);
+    if (this.currentMonth === 11) {
+      this.date.setFullYear(this.date.getFullYear() - 1);
+    }
+    this.queryData();
   }
 
   onClickNextMonth(): void {
-    this.date.setMonth(this.currentMonth + 1);
-    this.currentMonth = this.date.getMonth();
-    this.queryChart();
+    this.currentMonth = this.getNextMonth(this.currentMonth);
+    this.date.setMonth(this.currentMonth);
+    if (this.currentMonth === 0) {
+      this.date.setFullYear(this.date.getFullYear() + 1);
+    }
+    this.queryData();
+  }
+
+  getPrevMonth(month): number {
+    let date = new Date();
+    date.setDate(1);
+    date.setMonth(month - 1);
+    return date.getMonth();
+  }
+
+  getNextMonth(month): number {
+    let date = new Date();
+    date.setDate(1);
+    date.setMonth(month + 1);
+    return date.getMonth();
   }
 
 }
--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { OUserInfoService } from 'ontimize-web-ngx';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'home',
@@ -10,20 +9,7 @@ import { OUserInfoService } from 'ontimize-web-ngx';
   },
   encapsulation: ViewEncapsulation.None
 })
-export class HomeComponent implements OnInit {
-
-  public username: string;
-
-  constructor(
-    private userService: OUserInfoService
-  ) { }
-
-  ngOnInit() {
-    let userInfo = this.userService.getUserInfo();
-    if (userInfo) {
-      this.username = userInfo.username;
-    }
-  }
+export class HomeComponent {
 
   calculateUserBalance(data: any[]): number {
     return data.map(a => a.BALANCE).reduce((a, b) => a + b, 0);
